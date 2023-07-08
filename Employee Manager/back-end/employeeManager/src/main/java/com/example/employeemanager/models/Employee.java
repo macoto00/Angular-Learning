c84f1c28ea11d7a9460f5fb9773b4e7a01f1f91e
TomasMacoun <macount@seznam.cz>
2023-07-08 19:50:51 +0200

    Create Employee.java

---
+++ Employee Manager/back-end/employeeManager/src/main/java/com/example/employeemanager/models/Employee.java
@@ -0,0 +1,29 @@
+package com.example.employeemanager.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+}

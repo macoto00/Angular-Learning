aa27b4b225a52c78983a77ecd8d046efcc692831
TomasMacoun <macount@seznam.cz>
2023-07-08 20:57:21 +0200

    Create EmployeeRepository.java

---
+++ Employee Manager/back-end/employeeManager/src/main/java/com/example/employeemanager/repositories/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.example.employeemanager.repositories;
+
+import com.example.employeemanager.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}

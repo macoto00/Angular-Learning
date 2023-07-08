1f05866c222d19a43184340fd154bb913f8e87e7
TomasMacoun <macount@seznam.cz>
2023-07-08 07:09:50 +0200

    Create ResourceNotFoundException.java

---
+++ Employee App/Backend/employeeApp/src/main/java/com/example/employeeapp/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.employeeapp.Exceptions;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@AllArgsConstructor
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+}

82c01de4f0c9b71676bd4611f8a9a27bc00f833d
TomasMacoun <macount@seznam.cz>
2023-07-09 22:14:56 +0200

    Initial commit

---
+++ Employee Manager/back-end/logindAndRegistration/src/test/java/com/example/logindandregistration/LogindAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logindandregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogindAndRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

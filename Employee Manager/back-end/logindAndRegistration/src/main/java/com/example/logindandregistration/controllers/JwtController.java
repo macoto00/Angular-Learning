ce06b71a2d4b35e63733cd8a67d78ae1989f2d69
TomasMacoun <macount@seznam.cz>
2023-07-09 22:15:25 +0200

    controllers

---
+++ Employee Manager/back-end/logindAndRegistration/src/main/java/com/example/logindandregistration/controllers/JwtController.java
@@ -0,0 +1,23 @@
+package com.example.logindandregistration.controllers;
+
+import com.example.logindandregistration.models.JwtRequest;
+import com.example.logindandregistration.models.JwtResponse;
+import com.example.logindandregistration.services.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping({"/authenticate"})
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        return jwtService.createJwtToken(jwtRequest);
+    }
+}
---
+++ Employee Manager/back-end/logindAndRegistration/src/main/java/com/example/logindandregistration/controllers/RoleController.java
@@ -0,0 +1,20 @@
+package com.example.logindandregistration.controllers;
+
+import com.example.logindandregistration.models.Role;
+import com.example.logindandregistration.services.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping({"/createNewRole"})
+    public Role createNewRole(@RequestBody Role role) {
+        return roleService.createNewRole(role);
+    }
+}

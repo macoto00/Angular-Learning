5ac1f1ad009c8eda0f354f3d54c29b462e74e36b
TomasMacoun <macount@seznam.cz>
2023-07-09 10:55:27 +0200

    Update ApiController.java

--- Employee Manager/back-end/employeeManager/src/main/java/com/example/employeemanager/controllers/ApiController.java
+++ Employee Manager/back-end/employeeManager/src/main/java/com/example/employeemanager/controllers/ApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/employee")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ApiController {
     private final EmployeeService employeeService;
 
